9881e64282d43961abb0fcf5b16f5db7fd30d002
gerardfp <gerardfp@gmail.com>
2021-02-24 22:00:54 +0100

    .

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const cmd = async () => {
+  var ret = "mmm";
+  const { stdout, stderr } = await exec("java --version");
+  if (stderr) {
+      return stderr;
+  }
+  return stdout;
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -10,4 +22,6 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/test', (req, res) => res.send("test"))
+  .get('/test2', (req, res) => res.send("test2"))
+  .get('/java', async (req, res) => { const p = await cmd(); res.send(p)})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
