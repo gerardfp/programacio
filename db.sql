375698fe053808003fbd46d34a71567d8fd12bf9
gerardfp <gerardfp@gmail.com>
2021-02-26 22:28:16 +0100

    .

--- db.sql
+++ db.sql
@@ -4,4 +4,6 @@ CREATE TABLE users (id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY, emai
 
 INSERT INTO users (email, password, admin) VALUES ('admin', crypt('admin', gen_salt('bf', 8)), TRUE);
 
+INSERT INTO users (email, password, admin) VALUES ('user', crypt('user', gen_salt('bf', 8)), FALSE);
+
 SELECT * FROM users WHERE email = lower('nick@example.com') AND password = crypt('12345', password);
